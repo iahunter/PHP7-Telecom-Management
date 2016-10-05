b6c4953d6163b30892a7c8967508e5527e7a8b85
iahunter <travis.riesenberg@kiewit.com>
2016-10-05 16:35:02 -0500

    Adding CallManager Commands

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\Inspire::class,
         Commands\DidScan\Callmanager::class,
+		Commands\CallManager\Site911Local::class,
     ];
 
     /**

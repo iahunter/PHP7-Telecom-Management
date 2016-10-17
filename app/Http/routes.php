fa79d2b78b1ecc7f0fc32b0981f149e8ac738bb4
iahunter <travis.riesenberg@kiewit.com>
2016-10-17 11:03:37 -0500

    Added parameters to Swagger API Documentation

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -28,45 +28,53 @@ $api->version('v1', function ($api) {
         return "Hello world - demo app!\n";
     });
 
-    /*
+    /**
      * @SWG\Info(title="Phone Number API", version="0.1")
      **/
 
 
     // Authenticate returns a JWT upon success to authenticate additional API calls.
 
-    /*
-     * @SWG\Get(
-     *     path="/telephony/api/authenticate",
-     *     tags={"Authenticate"},
-     *     summary="Get JSON web token by TLS client certificate authentication",
-     *     description="",
-     *     operationId="authenticate",
-     *     consumes={"application/json"},
-     *     produces={"application/json"},
-     *     @SWG\Response(
-     *         response=200,
-     *         description="successful operation",
-     *     )
-     * )
-     **/
+	/**
+	 * @SWG\Get(
+	 *     path="/telephony/api/authenticate",
+	 *     tags={"Authentication"},
+	 *     summary="Get JSON web token by TLS client certificate authentication",
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Authentication succeeded",
+	 *         ),
+	 *     ),
+	 * )
+	 **/
     $api->get('authenticate', 'App\Http\Controllers\Auth\AuthController@authenticate');
 
-    /*
-     * @SWG\Post(
-     *     path="/telephony/api/authenticate",
-     *     tags={"Authenticate"},
-     *     summary="Get JSON web token by LDAP user authentication",
-     *     description="",
-     *     operationId="authenticate",
-     *     consumes={"application/json"},
-     *     produces={"application/json"},
-     *     @SWG\Response(
-     *         response=200,
-     *         description="successful operation",
-     *     )
-     * )
-     **/
+	/**
+	 * @SWG\Post(
+	 *     path="/telephony/api/authenticate",
+	 *     tags={"Authentication"},
+	 *     summary="Get JSON web token by LDAP user authentication",
+	 *     @SWG\Parameter(
+	 *         name="username",
+	 *         in="formData",
+	 *         description="LDAP username",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="password",
+	 *         in="formData",
+	 *         description="LDAP password",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Response(
+	 *         response=200,
+	 *         description="Authentication succeeded",
+	 *         ),
+	 *     ),
+	 * )
+	 **/
     $api->post('authenticate', 'App\Http\Controllers\Auth\AuthController@authenticate');
 
 
@@ -80,7 +88,7 @@ $api->version('v1', function ($api) {
     /********************************
         DID Block App routes
     ********************************/
-    /*
+    /**
      * @SWG\Post(
      *     path="/telephony/api/didblock",
      *     tags={"Did Block"},
@@ -89,6 +97,62 @@ $api->version('v1', function ($api) {
      *     operationId="createDidblock",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="name",
+	 *         in="formData",
+	 *         description="Name of New Block",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="carrier",
+	 *         in="formData",
+	 *         description="Carrier Information",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="comment",
+	 *         in="formData",
+	 *         description="Comment",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="country_code",
+	 *         in="formData",
+	 *         description="Country Code",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *	   @SWG\Parameter(
+	 *         name="start",
+	 *         in="formData",
+	 *         description="Range Start",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *	   @SWG\Parameter(
+	 *         name="end",
+	 *         in="formData",
+	 *         description="Range End",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="type",
+	 *         in="formData",
+	 *         description="public or private",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="reserved",
+	 *         in="formData",
+	 *         description="Automation Only",
+	 *         required=false,
+	 *         type="boolean"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -97,15 +161,22 @@ $api->version('v1', function ($api) {
      **/
     $api->post('didblock', 'App\Http\Controllers\Didcontroller@createDidblock');
 
-    /*
+    /**
      * @SWG\Get(
-     *     path="/telephony/api/didblock{id}",
+     *     path="/telephony/api/didblock/{id}",
      *     tags={"Did Block"},
      *     summary="Get DID Block by ID",
      *     description="",
      *     operationId="getDidblock",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of block id",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -115,7 +186,7 @@ $api->version('v1', function ($api) {
     $api->get('didblock/{id}', 'App\Http\Controllers\Didcontroller@getDidblock');
 
 
-    /*
+    /**
      * @SWG\Get(
      *     path="/telephony/api/didblock",
      *     tags={"Did Block"},
@@ -133,15 +204,43 @@ $api->version('v1', function ($api) {
      **/
     $api->get('didblock', 'App\Http\Controllers\Didcontroller@listDidblock');
 
-    /*
+    /**
      * @SWG\Put(
-     *     path="/telephony/api/didblock{id}",
+     *     path="/telephony/api/didblock/{id}",
      *     tags={"Did Block"},
      *     summary="Update DID Block by ID for authorized user",
      *     description="",
      *     operationId="updateDidblock",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of block id",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="name",
+	 *         in="formData",
+	 *         description="Name of New Block",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="carrier",
+	 *         in="formData",
+	 *         description="Carrier Information",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="comment",
+	 *         in="formData",
+	 *         description="Comment",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -151,15 +250,22 @@ $api->version('v1', function ($api) {
      **/
     $api->put('didblock/{id}', 'App\Http\Controllers\Didcontroller@updateDidblock');
 
-    /*
+    /**
      * @SWG\Delete(
-     *     path="/telephony/api/didblock{id}",
+     *     path="/telephony/api/didblock/{id}",
      *     tags={"Did Block"},
      *     summary="Delete DID Block by ID for authorized user",
      *     description="This deletes the block and its child Dids",
      *     operationId="updateDidblock",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of block to Delete",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -170,7 +276,7 @@ $api->version('v1', function ($api) {
     $api->delete('didblock/{id}', 'App\Http\Controllers\Didcontroller@deleteDidblock');
 
     // List DIDs by block id
-    /*
+    /**
      * @SWG\Get(
      *     path="/telephony/api/didblock/{id}/dids",
      *     tags={"Did"},
@@ -179,6 +285,13 @@ $api->version('v1', function ($api) {
      *     operationId="listDidbyBlockID",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of block id",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -193,7 +306,7 @@ $api->version('v1', function ($api) {
     // $api->post('did', 'App\Http\Controllers\Didcontroller@createDid'); // Individual DID creation not allowed.
         // List DIDs by block id
 
-    /*
+    /**
      * @SWG\Get(
      *     path="/telephony/api/did/id/{id}",
      *     tags={"Did"},
@@ -202,6 +315,13 @@ $api->version('v1', function ($api) {
      *     operationId="getDid",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of did id",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -211,7 +331,7 @@ $api->version('v1', function ($api) {
      **/
     $api->get('did/id/{id}', 'App\Http\Controllers\Didcontroller@getDid');
 
-    /*
+    /**
      * @SWG\Get(
      *     path="/telephony/api/did/number/{number}",
      *     tags={"Did"},
@@ -220,6 +340,13 @@ $api->version('v1', function ($api) {
      *     operationId="getDid",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="number",
+	 *         in="path",
+	 *         description="Search for Number",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -229,7 +356,7 @@ $api->version('v1', function ($api) {
      **/
     $api->get('did/number/{number}', 'App\Http\Controllers\Didcontroller@searchDidNumber');
 
-    /*
+    /**
      * @SWG\Get(
      *     path="/telephony/api/did/searchbyparent/{parentid}/{column}/{search}",
      *     tags={"Did"},
@@ -238,6 +365,27 @@ $api->version('v1', function ($api) {
      *     operationId="getDid",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="parentid",
+	 *         in="path",
+	 *         description="ID of parent",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="column",
+	 *         in="path",
+	 *         description="Column to Search",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="search",
+	 *         in="path",
+	 *         description="Search String",
+	 *         required=true,
+	 *         type="string"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",
@@ -247,25 +395,8 @@ $api->version('v1', function ($api) {
      **/
     $api->get('did/searchbyparent/{parentid}/{column}/{search}', 'App\Http\Controllers\Didcontroller@searchDidbyParent');
 
-    /*
-     * @SWG\Get(
-     *     path="/telephony/api/did",
-     *     tags={"Did"},
-     *     summary="List DIDs for authorized user",
-     *     description="This can be huge and need to add pagination",
-     *     operationId="listDid",
-     *     consumes={"application/json"},
-     *     produces={"application/json"},
-     *     @SWG\Response(
-     *         response=200,
-     *         description="successful operation",
 
-     *     ),
-     * )
-     **/
-    $api->get('did', 'App\Http\Controllers\Didcontroller@listDid');
-
-    /*
+    /**
      * @SWG\Put(
      *     path="/telephony/api/did/{id}",
      *     tags={"Did"},
@@ -274,6 +405,27 @@ $api->version('v1', function ($api) {
      *     operationId="listDid",
      *     consumes={"application/json"},
      *     produces={"application/json"},
+	 *     @SWG\Parameter(
+	 *         name="id",
+	 *         in="path",
+	 *         description="ID of did",
+	 *         required=true,
+	 *         type="integer"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="name",
+	 *         in="formData",
+	 *         description="Name of New Block",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
+	 *     @SWG\Parameter(
+	 *         name="status",
+	 *         in="formData",
+	 *         description="Available, Reserved",
+	 *         required=false,
+	 *         type="string"
+	 *     ),
      *     @SWG\Response(
      *         response=200,
      *         description="successful operation",

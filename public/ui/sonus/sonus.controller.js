663a0fc9de6a1411313b93580e347c6810abd857
iahunter <travis.riesenberg@kiewit.com>
2017-01-25 11:42:01 -0600

    Added autorefresh to Sonus Call Summary UI

--- public/ui/sonus/sonus.controller.js
+++ public/ui/sonus/sonus.controller.js
@@ -1,10 +1,23 @@
 angular
 	.module('app')
-	.controller('Sonus.IndexController', ['SonusService', '$location', '$state', function(SonusService, $location, $state) {
+	.controller('Sonus.IndexController', ['SonusService', '$location', '$timeout', '$state', '$scope', function(SonusService, $location, $timeout, $state, $scope) {
 	
 		var vm = this;
 		
-		//initController();
+		
+		/* Don't think we need this anymore. Keeping for example watch. 
+		$scope.$watch(function() {
+            return vm.callarray;
+        }, function(current, original) {
+			
+			vm.callarray = current;
+            console.log('vm.callarray was %s', original);
+            console.log('vm.callarray is now %s', current);
+
+        });
+		*/
+			
+		initController();
 		
 		vm.siteForm = {};
 		
@@ -16,53 +29,57 @@ angular
 		//vm.sites = [{}];
 		vm.loading = true;
 		
-
-		vm.getdidblocks = SonusService.listactivecalls()
-			.then(function(res){
-				var calls = res.data;
-				vm.callcount = 0;
-				// Create our blank simple array for datatimegrps 
-				vm.callarray = [];
-				
-				//console.log(calls);
-				// Loop thru and append to a simple array so we can do a simple select on it with ng-options.
-				angular.forEach(calls, function(value, key) {
-					  angular.forEach(value, function(v, k) {
-							  if (v == null){
-								  //alert("No Active Calls");
-								  vm.loading = false;
-							  }
+		function initController() {
+			vm.getactivecalls = SonusService.listactivecalls()
+				.then(function(res){
+					var calls = res.data;
+					vm.callcount = 0;
+					// Create our blank simple array for datatimegrps 
+					vm.callarray = [];
+					
+					//console.log(calls);
+					// Loop thru and append to a simple array so we can do a simple select on it with ng-options.
+					angular.forEach(calls, function(value, key) {
+						  angular.forEach(value, function(v, k) {
+								  if (v == null){
+									  //alert("No Active Calls");
+									  vm.loading = false;
+								  }
+								  
+								  else{
+									  angular.forEach(v, function(call, object) {
+										  //console.log(call.calledNumber);
+										  //console.log(call);
+										  call['SBC'] = key;
+										  vm.callarray.push(call);
+										  vm.callcount = vm.callcount + 1;
+										  vm.noactivecalls = false;
+									  });
+								  }
 							  
-							  else{
-								  angular.forEach(v, function(call, object) {
-									  console.log(call.calledNumber);
-									  console.log(call);
-									  call['SBC'] = key;
-									  vm.callarray.push(call);
-									  vm.callcount = vm.callcount + 1;
-								  });
-							  }
-						  
-						});
+							});
+					});
+					
+					
+					//console.log(vm.callarray);
+					
+					
+					if(vm.callarray.length == 0){
+						// If no active calls returned then set the noactivecalls variable to display the message to the user. 
+						vm.noactivecalls = true;
+					}
+					
+					// Stop Loading 
+					vm.loading = false;
+					
+					$timeout(initController,5000); 
+						
+				}, function(err){
+					alert(err);
 				});
-				
-				
-				console.log(vm.callarray);
-				
-				if(vm.callarray.length == 0){
-					// If no active calls returned then set the noactivecalls variable to display the message to the user. 
-					vm.noactivecalls = true;
-				}
-				
-				// Stop Loading 
-				vm.loading = false;
-					
-					
-			}, function(err){
-				alert(err);
-			});
+		}
 		
-		
+		/*
 		function initController() {
 			SonusService.listactivecalls(function (result) {
 				console.log('callback from SonusService.listactivecalls responded ' + result);
@@ -76,7 +93,7 @@ angular
 
 			});
 		}
-		
+		*/
 
 	}])
 

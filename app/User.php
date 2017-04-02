32a0adf7fdf3f58e61e6d1900efbd3ff6e91956c
iahunter <travisriesenberg@gmail.com>
2017-04-02 21:37:14 +0000

    Apply fixes from StyleCI

--- app/User.php
+++ app/User.php
@@ -37,45 +37,42 @@ class User extends Authenticatable implements \Tymon\JWTAuth\Contracts\JWTSubjec
         // Add custom properties to the token here. This can be used to send the browser its permissions.
         $claims = [];
         $claims['permissions'] = [];
-		$claims['permissions']['read'] = [];
-		$claims['permissions']['create'] = [];
-		$claims['permissions']['update'] = [];
-		$claims['permissions']['delete'] = [];
-		
-		/*****************************************************
-							Did Block
-		*****************************************************/
-		// Check Role of user
+        $claims['permissions']['read'] = [];
+        $claims['permissions']['create'] = [];
+        $claims['permissions']['update'] = [];
+        $claims['permissions']['delete'] = [];
+
+        /*****************************************************
+                            Did Block
+        *****************************************************/
+        // Check Role of user
         if ($this->can('read', Didblock::class)) {
             $claims['permissions']['read']['Didblock'] = true;
         }
-		
-		// Check Role of user
+
+        // Check Role of user
         if ($this->can('create', Didblock::class)) {
             $claims['permissions']['create']['Didblock'] = true;
         }
-		
-		// Check Role of user
+
+        // Check Role of user
         if ($this->can('update', Didblock::class)) {
             $claims['permissions']['update']['Didblock'] = true;
         }
-		
-		// Check Role of user
+
+        // Check Role of user
         if ($this->can('delete', Didblock::class)) {
             $claims['permissions']['delete']['Didblock'] = true;
         }
-		
-		/*****************************************************
-							Site
-		*****************************************************/
+
+        /*****************************************************
+                            Site
+        *****************************************************/
 
         // Check Role of user
         if ($this->can('read', Site::class)) {
             $claims['permissions']['read']['Site'] = true;
         }
-
-        
-		
 
         // Check Role of user
         if ($this->can('read', Cucmsiteconfigs::class)) {

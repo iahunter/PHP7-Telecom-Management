0fe8e67c23c63627d02e03b3c73964f7a8fe6236
iahunter <travisriesenberg@gmail.com>
2017-04-28 19:06:47 +0000

    Apply fixes from StyleCI

--- database/migrations/2017_04_28_154758_extend_users_table_with_samaccountname.php
+++ database/migrations/2017_04_28_154758_extend_users_table_with_samaccountname.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Migrations\Migration;
 
 class ExtendUsersTableWithSamaccountname extends Migration
 {
-     /**
+    /**
      * Run the migrations.
      *
      * @return void

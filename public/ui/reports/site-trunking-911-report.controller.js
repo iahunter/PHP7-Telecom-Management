01d03b2edd8e2036c9910203ac3985ba330240f6
iahunter <travis.riesenberg@kiewit.com>
2017-05-20 15:06:07 -0500

    chart stuff on trunking report

--- public/ui/reports/site-trunking-911-report.controller.js
+++ public/ui/reports/site-trunking-911-report.controller.js
@@ -1,6 +1,6 @@
 angular
 	.module('app')
-	.controller('siteTrunking911Report.IndexController', ['cucmReportService', 'PageService','$location', '$state', '$stateParams', function(cucmReportService, PageService, $location, $state, $stateParams) {
+	.controller('siteTrunking911Report.IndexController', ['cucmReportService', 'PageService','$location', '$state', '$scope', '$stateParams', function(cucmReportService, PageService, $location, $state, $scope,$stateParams) {
 	
 		var vm = this;
 
@@ -36,6 +36,69 @@ angular
 					//console.log(vm.message);
 				}else{
 					vm.sites = res.data.response;
+					
+					// Use this to change chart colors. 
+					//Chart.defaults.global.colors = ['#FD1F5E','#1EF9A1','#7FFD1F','#68F000'];
+					vm.e911stats = res.data.e911stats
+					
+					//console.log(block.stats);
+					vm.e911 = [];
+					vm.e911['chartlabels'] = [];
+					vm.e911['chartdata'] = [];
+					//vm.didblock['chartseries'] = [];
+					
+					for(var key in vm.e911stats){
+						//console.log(key);
+						//console.log(vm.didblock.stats[key]);
+						
+						vm.e911['chartlabels'].push(key);
+						vm.e911['chartdata'].push(vm.e911stats[key]);
+						//vm.didblock['chartseries'].push(key);
+						
+					}
+					
+					
+					// Enable the Options to be generated for the chart. 
+					//vm.didblock.chartoptions = { responsive: true, legend: { display: true}, title: {display:true, text:'Number Block Usage'}};
+					vm.e911.chartoptions = { responsive: true, scales: {
+																				xAxes: [{
+																					ticks: {
+																						beginAtZero:true
+																					}
+																				}]
+																			}};
+					
+					//Chart.defaults.global.colors = ['#000000','#1EF9A1','#7FFD1F','#68F000'];
+					vm.trunkingstats = res.data.trunkingstats
+					
+					//console.log(block.stats);
+					vm.trunking = [];
+					vm.trunking['chartlabels'] = [];
+					vm.trunking['chartdata'] = [];
+					//vm.didblock['chartseries'] = [];
+					
+					for(var key in vm.trunkingstats){
+						//console.log(key);
+						//console.log(vm.didblock.stats[key]);
+						
+						vm.trunking['chartlabels'].push(key);
+						vm.trunking['chartdata'].push(vm.trunkingstats[key]);
+						//vm.didblock['chartseries'].push(key);
+						
+					}
+					
+					
+					// Enable the Options to be generated for the chart. 
+					//vm.didblock.chartoptions = { responsive: true, legend: { display: true}, title: {display:true, text:'Number Block Usage'}};
+					vm.trunking.chartoptions = { responsive: true, scales: {
+																				xAxes: [{
+																					ticks: {
+																						beginAtZero:true
+																					}
+																				}]
+																			}};
+					
+					console.log(vm.trunking)
 					vm.loading = false;
 				}
 				

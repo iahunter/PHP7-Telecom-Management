dbb5e5cd090c9151cfbeef1d1782cd78f2ef4699
iahunter <travis.riesenberg@kiewit.com>
2017-05-24 15:47:32 -0500

    got email notifications working for infrastructure icmp monitoring

--- app/Console/Commands/Monitoring/PingScanInfrastructure.php
+++ app/Console/Commands/Monitoring/PingScanInfrastructure.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\TelecomInfrastructure;
 use App\Ping;
 use Illuminate\Console\Command;
+use Mail;
 
 class PingScanInfrastructure extends Command
 {
@@ -50,9 +51,11 @@ class PingScanInfrastructure extends Command
 			$host_status = Ping::pinghost($ip);
 			if($host_status['result'] == "echo reply"){
 				$device_status = true;
+				$friendly_status = "up";
 			}
 			else{
 				$device_status = false;
+				$friendly_status = "down";
 			}
 			
 			 // Find record by id
@@ -61,11 +64,32 @@ class PingScanInfrastructure extends Command
 			// If the Status change does not match what is currently set in the Database. Update the Database with the new status. 
 			if($device->ip_reachable != $device_status){
 				$device->ip_reachable = $device_status;
+				
+				
+				$time = Carbon::now().PHP_EOL;
+				$hostdata = $hostname." | ".$ip.PHP_EOL;
+				$change = "Status Change To: ".$friendly_status.PHP_EOL;
+				
+				print $time.$hostdata.$change;
+				
+				$data = [
+						'time' 		=> $time,
+						'host' 		=> $host,
+						'status' 	=> $friendly_status,
+						];
+			
+				Mail::send(['html'=>'email'], $data, function($message){
+						$message->subject("Telecom Management Alert - Device Status Change!")
+								->from([env('MAIL_FROM_ADDRESS')])
+								->to([env('ONCALL_EMAIL_TO'), env('ONCALL_EMAIL_TO')])
+								->bcc([env('BACKUP_EMAIL_TO'), env('BACKUP_EMAIL_TO')]);
+				});
+				
+				print "Email sent to ".env('ONCALL_EMAIL_TO').PHP_EOL;
+				print "Email sent to ".env('BACKUP_EMAIL_TO').PHP_EOL;
+				
 				$device->updated_by = 'Monitoring Change';
 				$device->save();
-				print Carbon::now().PHP_EOL;
-				print $hostname." | ".$ip.PHP_EOL;
-				print "Status Change: ".$host_status['result'].PHP_EOL;
 			}
 		}
 		
---
+++ resources/views/email.blade.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Device Status Change</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+   
+    </head>
+    <body>
+		<legend>
+			<h1>Telecom Management - Device Status Change!</h1>
+			<h3>{{ $host->hostname}} is: {{$status}}</h3>
+		</legend>
+        <div class="jumbotron">
+			<div class="panel panel-default" style="box-shadow: 1px 1px 5px grey;">
+				<div class="table-responsive">    
+					<table class="table table-striped table-condensed table-bordered table-hover">
+						<tbody style="font-size: 12px;">
+							<!--List View-->
+							<tr><td><b>Time: </b> {{ $time}}</b></td></tr>
+							<tr><td><b>Status: </b> {{ $status}}</b></td></tr>
+							<tr><td><b>Hostname: </b> {{ $host->hostname}}</b></td></tr>
+							<tr><td><b>Comment: </b>{{ $host->comment}}</td></tr>
+							<tr><td><b>Function: </b>{{ $host->function}}</td></tr>
+							<tr><td><b>Role: </b>{{ $host->role}}</a></td></tr>
+							<tr><td><b>Manufacturer: </b>{{ $host->manufacturer}}</td></tr>
+							<tr><td><b>Application: </b>{{ $host->application}}</td></tr>
+							<tr><td><b>Software_version: </b>{{ $host->software_version}}</td></tr>
+							<tr><td><b>OS: </b>{{ $host->os}}</td></tr>
+							<tr><td><b>IP Address: </b>{{ $host->ip_address}}</td></tr>
+							<tr><td><b>URL: </b>{{ $host->mgmt_url}}</td></tr>
+							<tr><td><b>Location: </b>{{ $host->location}}</td></tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+        </div>
+    </body>
+</html>

9c77deb0bb5a8a95fbcd937fcd349e470189e24a
iahunter <travis.riesenberg@kiewit.com>
2017-05-30 21:32:35 -0500

    formatting

--- public/admin/home/home.controller.js
+++ public/admin/home/home.controller.js
@@ -39,6 +39,7 @@
 
 		// Match the window permission set in login.js and app.js - may want to user a service or just do an api call to get these. will decide later. 
 		vm.permissions = window.telecom_mgmt_permissions;
+		vm.userid = window.telecom_user;
 
 		vm.messages = 'Loading Userinfo...';
 		vm.userinfo = {};

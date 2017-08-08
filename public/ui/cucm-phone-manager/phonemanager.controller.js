3c0a60caa1c3afd6f84ab6985c63702a755a089b
iahunter <travis.riesenberg@kiewit.com>
2017-08-08 10:55:36 -0500

    updates to macd views

--- public/ui/cucm-phone-manager/phonemanager.controller.js
+++ public/ui/cucm-phone-manager/phonemanager.controller.js
@@ -31,6 +31,7 @@ angular
 		vm.sitecode = id;
 		
 		var phone = $stateParams.name;
+		vm.name = $stateParams.name
 		
 		// Match the window permission set in login.js and app.js - may want to user a service or just do an api call to get these. will decide later. 
 		vm.permissions = window.telecom_mgmt_permissions;

263c0f3fc1e6072e84cbc4d4a30060eda3ab1f63
iahunter <travisriesenberg@gmail.com>
2017-08-08 20:04:23 +0000

    Apply fixes from StyleCI

--- app/Http/Controllers/CucmRealTimeController.php
+++ app/Http/Controllers/CucmRealTimeController.php
@@ -10,38 +10,37 @@ use Tymon\JWTAuth\Facades\JWTAuth;
 // CUCM Real Time API Testing
 
 class CucmRealTimeController extends Controller
-{	
-
-	public function __construct()
+{
+    public function __construct()
     {
         // Only authenticated users can make these calls
         $this->middleware('jwt.auth');
 
         // Construct new cucm object
-        $this->CucmRealTime = new CucmRealTime; 
+        $this->CucmRealTime = new CucmRealTime();
     }
-	
+
     public function get_phone_ip(Request $request)
     {
-		// First test for RIS API. Used to get IP for the Phone.
-		
+        // First test for RIS API. Used to get IP for the Phone.
+
         $user = JWTAuth::parseToken()->authenticate();
         // Check user permissions
         if (! $user->can('read', Cucmclass::class)) {
             abort(401, 'You are not authorized');
         }
-		
-		// Get name from $request;
-		$name = $request->name;
-		
-		$count = 0;
-		
-		// Search for name. 
-		$searchCriteria["SelectItem[$count]"]["Item"] = $name;
-		
-		$result = $this->CucmRealTime->getIPAddresses($searchCriteria);
 
-		$response = [
+        // Get name from $request;
+        $name = $request->name;
+
+        $count = 0;
+
+        // Search for name.
+        $searchCriteria["SelectItem[$count]"]['Item'] = $name;
+
+        $result = $this->CucmRealTime->getIPAddresses($searchCriteria);
+
+        $response = [
                     'status_code'    => 200,
                     'success'        => true,
                     'message'        => '',

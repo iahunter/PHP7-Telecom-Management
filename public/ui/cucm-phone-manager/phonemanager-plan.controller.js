bdd34a9e9ef9dfad6d99523687d1472eb84ef3e9
iahunter <travis.riesenberg@kiewit.com>
2017-08-09 12:52:01 -0500

    ui changes

--- public/ui/cucm-phone-manager/phonemanager-plan.controller.js
+++ public/ui/cucm-phone-manager/phonemanager-plan.controller.js
@@ -29,6 +29,9 @@ angular
 		var id = $stateParams.id;
 		vm.sitecode = id;
 		
+		vm.deviceForm = {}
+		vm.deviceForm.sitecode = id; 
+		
 		// Match the window permission set in login.js and app.js - may want to user a service or just do an api call to get these. will decide later. 
 		vm.permissions = window.telecom_mgmt_permissions;
 

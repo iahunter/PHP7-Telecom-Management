32a0f402be8aaee3552e5a492125971229bff559
root <root@nettlrwld001.kiewitplaza.com>
2019-09-13 20:17:07 +0000

    Fixing Migrations for redeployment

---
+++ database/migrations/2019_09_13_200111_abilities_extend_entityid.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AbilitiesExtendEntityid extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+	 
+	/* Fix for error:
+		[Doctrine\DBAL\DBALException]                                                                    
+		Unknown database type json requested, Doctrine\DBAL\Platforms\MySqlPlatform may not support it. 
+		https://stackoverflow.com/questions/48256476/unknown-database-type-json-requested-doctrine-dbal-platforms-mysql57platform-m
+	*/
+	public function __construct()
+	{
+		DB::getDoctrineSchemaManager()->getDatabasePlatform()->registerDoctrineTypeMapping('json', 'string');
+	}
+	
+    public function up()
+    {
+        //
+        Schema::table('abilities', function ($table) {
+            $table->string('entity_id', 255)->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('abilities', function ($table) {
+            $table->integer('entity_id')->unsigned()->nullable()->change();
+        });
+    }
+}

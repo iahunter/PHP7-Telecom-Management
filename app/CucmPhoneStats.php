dcbfcbc0f6b8397a41ce7c4c9475d62088e447b4
iahunter <root@knenetwld002>
2020-03-15 17:36:57 -0500

    working on building cucm phone stats data

---
+++ app/CucmPhoneStats.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CucmPhoneStats extends Model
+{
+    protected $table = 'cucmphonestats';
+    protected $fillable = ['type', 'total', 'registered', 'stats', 'json'];
+
+    protected $casts = [
+        'stats'    => 'array',
+		'json'    => 'array',
+    ];
+	
+
+    protected $attributes = [
+        'json' => '{}',
+    ];
+}
---
+++ database/migrations/2020_03_15_221404_create_cucm_phone_stats_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCucmPhoneStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+		Schema::create('cucmphonestats', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('type')->nullable();
+			$table->integer('total')->nullable();
+			$table->integer('registered')->nullable();				
+			$table->json('stats')->nullable();                      // JSON Custom Field Data
+			$table->json('json')->nullable();                       // JSON Custom Field Data
+			$table->timestamps();                       			// Time Stamps
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('cucmphonestats');
+    }
+}
